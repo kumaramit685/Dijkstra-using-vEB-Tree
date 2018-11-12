4d26ac711a3853f825246bb91dffe6fc0d6dbf79
kumaramit685 <kumaramit685@gmail.com>
2018-11-12 08:52:27 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,13 @@
+g++ Dijkstra_binomial_fibnacci.cpp -o Dijkstra_binomial_fibnacci.out
+./Dijkstra_binomial_fibnacci.out testcase1.txt
+./Dijkstra_binomial_fibnacci.out testcase2.txt
+./Dijkstra_binomial_fibnacci.out testcase3.txt
+./Dijkstra_binomial_fibnacci.out testcase4.txt
+g++ vEB_Dijkstra_tc1.cpp -o vEB_Dijkstra.out
+./vEB_Dijkstra.out
+g++ vEB_Dijkstra_tc2.cpp -o vEB_Dijkstra.out
+./vEB_Dijkstra.out
+g++ vEB_Dijkstra_tc3.cpp -o vEB_Dijkstra.out
+./vEB_Dijkstra.out
+g++ vEB_Dijkstra_tc4.cpp -o vEB_Dijkstra.out
+./vEB_Dijkstra.out
